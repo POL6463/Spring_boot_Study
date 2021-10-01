57ca0aa54c7e24c1ac4fdf642f9f4a8a372176b7
POL6463 <kevinkim0617kim@gmail.com>
2021-10-01 09:26:24 +0900

    네이버 로그인 하는중

--- src/test/java/com/study/springboot/web/HelloControllerTest.java
+++ src/test/java/com/study/springboot/web/HelloControllerTest.java
@@ -13,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.ResultActions;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest ()
